02e6e7b65e4f94eea941f068d4c94737ba64a17a
bradwhist <bradwhist@gmail.com>
2018-07-16 16:09:02 -0700

    documents

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-import http from 'http';
+// import http from 'http';
+//
+//
+//
+// http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(1337, '127.0.0.1');
+//
+// console.log('Server running at http://127.0.0.1:1337/');
 
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(8080);
+io.on('connection', function(socket) {
+  console.log('connected to socket')
+  socket.emit('connect', {hello: 'world' });
+  socket.on('cmd', function (data) {
+    console.log(data);
+  });
+});
